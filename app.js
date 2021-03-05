e667f5c8be4697869aab31eba1ca5663ca66ae02
Vladimir Levchenko <vladimir.levchenko@marketman.com>
2021-03-05 17:40:42 +0200

    dependencies changed

--- app.js
+++ app.js
@@ -7,10 +7,10 @@ const cors = require('cors');
 app.use(express.json());
 
 app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', 'http://192.168.1.179:8090');
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Credentials', 'true');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    res.header('Access-Control-Allow-Credentials', 'true');
 
     if (req.method === 'OPTIONS') {
         return res.status(200).json({});
