0a5c0be8f572114a99e3bc06923763a476c22c2f
Vladimir Levchenko <vladimir.levchenko@marketman.com>
2021-03-05 17:36:22 +0200

    origin added

--- app.js
+++ app.js
@@ -5,14 +5,11 @@ const fs = require('fs');
 const cors = require('cors');
 
 app.use(express.json());
-app.use(cors({
-    credentials: true,
-    origin: true
-}))
-/*app.use(function (req, res, next) {
+
+app.use(function (req, res, next) {
     res.header('Access-Control-Allow-Origin', 'http://192.168.1.179:8090');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.header('Access-Control-Allow-Credentials', 'true');
 
     if (req.method === 'OPTIONS') {
@@ -20,7 +17,7 @@ app.use(cors({
     }
 
     next();
-});*/
+});
 
 app.post('/event', (req, res) => {
     console.log(req.body);
