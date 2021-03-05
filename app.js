4e999a73177abeec0ec71f4b52ea97173f69ec2d
Vladimir Levchenko <vladimir.levchenko@marketman.com>
2021-03-05 17:41:38 +0200

    cors plugin activated

--- app.js
+++ app.js
@@ -6,18 +6,24 @@ const cors = require('cors');
 
 app.use(express.json());
 
-app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Credentials', 'true');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+app.use(cors({
+    origin: true,
+    credentials: true
+}));
 
-    if (req.method === 'OPTIONS') {
-        return res.status(200).json({});
-    }
 
-    next();
-});
+// app.use(function (req, res, next) {
+//     res.header('Access-Control-Allow-Origin', '*');
+//     res.header('Access-Control-Allow-Credentials', 'true');
+//     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+//     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+//
+//     if (req.method === 'OPTIONS') {
+//         return res.status(200).json({});
+//     }
+//
+//     next();
+// });
 
 app.post('/event', (req, res) => {
     console.log(req.body);
