dd5d9e89cc7cb02654477379cc8820d45dc5ce0c
Vladimir Levchenko <vladimir.levchenko@marketman.com>
2021-03-05 17:02:50 +0200

    pdf generator added

--- app.js
+++ app.js
@@ -1,25 +1,37 @@
 const express = require('express');
 const app = express();
-const cors = require('cors');
+const pdf = require('html-pdf');
+const fs = require('fs');
 
 app.use(express.json());
-// app.use(cors())
-app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:8090');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.header('Access-Control-Allow-Credentials', 'true');
-
-    if (req.method === 'OPTIONS') {
-        return res.status(200).json({});
-    }
-
-    next();
-});
+app.use(require('cors')());
 
 app.post('/event', (req, res) => {
     console.log(req.body);
     res.status(201).end();
 });
+
+app.post('/generatePDF', (req, res) => {
+    const fileName = `print${Number(new Date())}.pdf`;
+    console.log('create pdf');
+    pdf.create(req.body.html, {
+        border: '10mm'
+    }).toFile(`./files/${fileName}`, function (err, result) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.status(200).json({
+                file: fileName
+            });
+            const to = setTimeout(() => {
+                fs.unlinkSync(result.filename);
+                clearTimeout(to);
+            }, 5000)
+        }
+    });
+});
+
+app.use('/files', express.static('./files'));
+
 
 module.exports = app;
